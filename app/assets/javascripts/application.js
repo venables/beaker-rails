cba6865cc71dade5e72432c1a901e1887c5f718f
Matt Venables <mattvenables@gmail.com>
2015-01-10 10:54:42 -0500

    Move index.html to StaticPages#home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,169 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require_tree .
+//= require_self
+
+'use strict';
+
+$(function() {
+  var Session = {
+    token: null,
+
+    signIn: function(token) {
+      Session.token = token;
+      Session.toggleViews();
+    },
+
+    signOut: function() {
+      Session.token = null;
+      Session.toggleViews();
+    },
+
+    toggleViews: function() {
+      if (Session.token) {
+        $('#not-authenticated').hide();
+        $('#authenticated').show();
+      } else {
+        $('#not-authenticated').show();
+        $('#authenticated').hide();
+      }
+    }
+  };
+
+  var api = {
+    headers: function() {
+      var result = {};
+
+      if (Session.token) {
+        result['Authorization'] =  'Token token=' + Session.token;
+      }
+
+      return result
+    },
+
+
+    perform: function(method, url, data, callback) {
+      // TODO: ADD HEADERS HERE
+      $.ajax({
+        type: method,
+        url: url,
+        data: data,
+        dataType: 'text',
+        headers: api.headers(),
+        success: function(data) {
+          var json = null;
+
+          try {
+            json = JSON.parse(data);
+          } catch(e) {}
+
+          api.handleSuccess(json);
+
+          if (callback) { callback(null, json); }
+        },
+        error: function(xhr) {
+          var json = null;
+
+          try {
+            json = JSON.parse(xhr.responseText);
+          } catch(e) {}
+
+          var errors = json && json.errors && json.errors.messages || [];
+          api.handleError(errors);
+          if (callback) { callback(errors); }
+        }
+      });
+    },
+
+    post: function(url, data, callback) {
+      api.perform('POST', url, data, callback);
+    },
+
+    put: function(url, data, callback) {
+      api.perform('PUT', url, data, callback);
+    },
+
+    del: function(url, callback) {
+      api.perform('DELETE', url, {}, callback);
+    },
+
+    handleSuccess: function(data) {
+      if (!data) { return; }
+
+      if (data.session && data.session.token) {
+        Session.signIn(data.session.token);
+      }
+    },
+
+    handleError: function(errors) {
+      if (errors.length > 0) {
+        alert(errors.join('. '));
+      }
+    }
+  };
+
+  $(document).on('submit', 'form#register', function(e) {
+    e.preventDefault();
+
+    api.post('/api/v1/users', { user: $(this).serializeObject() });
+  });
+
+  $(document).on('submit', 'form#signin', function(e) {
+    e.preventDefault();
+
+    api.post('/api/v1/sessions', $(this).serializeObject());
+  });
+
+  $(document).on('submit', 'form#forgot-password', function(e) {
+    e.preventDefault();
+
+    api.post('/api/v1/password_resets', $(this).serializeObject(), function(err) {
+      if (!err) {
+        $('form#reset-password').show();
+      }
+    });
+  });
+
+  $(document).on('submit', 'form#reset-password', function(e) {
+    e.preventDefault();
+
+    var data = $(this).serializeObject();
+    var token = data['token'];
+    delete data['token'];
+
+    api.put('/api/v1/password_resets/' + token, data, function(err) {
+      if (!err) {
+        $('form#reset-password').hide();
+        $('form#signin input#signin-email').val($('form#forgot-password input#forgot-password-email').val());
+        $('form#signin input#signin-password').focus();
+      }
+    });
+  });
+
+  $(document).on('submit', 'form#signout', function(e) {
+    e.preventDefault();
+
+    api.del('/api/v1/sessions', function(err) {
+      if (!err) {
+        Session.signOut();
+      }
+    });
+  });
+
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#home'
 
   namespace :api, defaults: { format: 'json' }, constraints: { format: 'json' } do
     namespace :v1 do
